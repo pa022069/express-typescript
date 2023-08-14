f7168c4762bdf1a7c3813e4424fba44268aec877
JeffreyWu <pa022069@gmail.com>
2023-08-14 18:05:06 +0800

    add swagger

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Node express with Typescript
+
+[Express/Swagger](https://nerd-corner.com/import-swagger-in-node-typescript-project/)
--- server.ts
+++ server.ts
@@ -4,8 +4,14 @@ import cors from "cors";
 import server from "./routes/server";
 import login from "./routes/login";
 import helmet from "helmet";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger.json";
 
 const app = express();
+
+// Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// Swagger
 
 app.use(helmet());
 app.use(
@@ -20,7 +26,7 @@ app.use(
 app.use(bodyparser.urlencoded({ limit: "3mb", extended: true }));
 app.use(bodyparser.json({ limit: "3mb" }));
 
-app.use("/server", server);
+app.use("/server", swaggerUi.serve, server);
 app.use("/login", login);
 
 app.listen(4002, function () {
